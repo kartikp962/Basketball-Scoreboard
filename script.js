03df27c126be686eb2780febc830f16dbbd4b66c
Kartik Pal <114161683+kartikp962@users.noreply.github.com>
2023-01-13 14:39:54 +0530

    Basketball Game Scoreboard made with HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,41 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+}
+
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+let guestStoreEl =document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreOne(){
+    guestScore += 1
+    guestStoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreTwo(){
+    guestScore += 2
+    guestStoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreThree(){
+    guestScore += 3
+    guestStoreEl.textContent = guestScore
+}
